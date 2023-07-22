8a70a1f130723c3de96e90a505cc3c54e6b90d43
vctrfrbrg <victor@frbrg.se>
2023-07-22 17:52:08 +0200

    Enable darkmode check

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import { Grid, Typography } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 const App = () => (
-  <Grid 
-    container
-    direction="column"
-    alignItems="center"
-    justifyContent="center"
-    height={"96vh"}
-  >
-    <Grid item>
-      <Typography variant='h1'>FRBRG Ventures</Typography>
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <Grid 
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      height={"96vh"}
+    >
+      <Grid item>
+        <Typography variant='h1'>FRBRG Ventures</Typography>
+      </Grid>
+      <Grid item>
+        <Typography>© 2023</Typography>
+      </Grid>
     </Grid>
-    <Grid item>
-      <Typography>© 2023</Typography>
-    </Grid>
-  </Grid>
-)
+    </ThemeProvider>
+);
 
 export default App;
