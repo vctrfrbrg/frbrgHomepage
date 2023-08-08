849b17b8bae18be74e9d312bed0a5aa6e8cde871
vctrfrbrg <victor@frbrg.se>
2023-08-08 15:13:11 +0200

    Update readme, add newest builds

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Hej!
+### FRBRG Ventures landing page
+We keep it simple here.
+
+Start with 
+
+```npm i```
+
+
+```npm start```
+
+from the root.
---
+++ build/precache-manifest.034cccb8e56374f1dfc0ba328804361e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cef13dfe38e1cef8ec8",
+    "url": "/frbrgHomepage/static/js/main.5cef13df.chunk.js"
+  },
+  {
+    "revision": "909ead1fac143a47aeb6",
+    "url": "/frbrgHomepage/static/js/1.909ead1f.chunk.js"
+  },
+  {
+    "revision": "2a39bc3dba0608f76eca",
+    "url": "/frbrgHomepage/static/js/runtime~main.2a39bc3d.js"
+  },
+  {
+    "revision": "22fa872bacdb16e9f75b6b8a5304686c",
+    "url": "/frbrgHomepage/index.html"
+  }
+];
